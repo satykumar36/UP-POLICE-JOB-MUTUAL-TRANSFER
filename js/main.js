4112ab1fa1519169a342e071e0e03cd7a44eb602
Satyendra kumar prajapati <satykumar36@gmail.com>
2024-10-11 00:35:25 +0530

    Code Updated

--- js/main.js
+++ js/main.js
@@ -38,8 +38,8 @@
         loop: true,
         nav : true,
         navText : [
-            '<i class="bi bi-arrow-left"></i>',
-            '<i class="bi bi-arrow-right"></i>'
+            '<i class="bi bi-arrow-right"></i>',
+            '<i class="bi bi-arrow-left"></i>'
         ],
     });
 
